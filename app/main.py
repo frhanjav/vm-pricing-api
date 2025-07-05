f8b4b85f8b368a7f9927c303d36971991c0aae16
frhanjav <farhan.javed006@gmail.com>
2025-07-05 14:28:38 +0530

    fix-deprecated

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.api.endpoints import router as api_router
 from app.core.config import settings
 from app.data.data_manager import data_manager
 from app.services.scheduler import scheduler, start_scheduler, stop_scheduler
 
-app = FastAPI(title=settings.APP_NAME)
-
-@app.on_event("startup")
-async def startup_event():
-    """
-    On startup, load initial data and start the background scheduler.
-    """
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up...")
     await data_manager.load_data()
     start_scheduler()
-
-@app.on_event("shutdown")
-def shutdown_event():
+    
+    yield 
+    
+    print("Shutting down...")
     stop_scheduler()
+
+app = FastAPI(title=settings.APP_NAME, lifespan=lifespan)
 
 app.include_router(api_router, prefix="/api/v1")
